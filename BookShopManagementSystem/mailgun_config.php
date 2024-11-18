b16603d60bd69ddb9b8ae7f070225c337b6b9399
BokCheongRoy <110295168+BokCheongRoy@users.noreply.github.com>
2024-11-18 13:40:08 +0800

    Add files via upload

---
+++ BookShopManagementSystem/mailgun_config.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+define('MAILGUN_API_KEY', '05b03980d241068295b12e7412658bf7-826eddfb-60451ddd');
+define('MAILGUN_DOMAIN', 'your-mailgun-domain');
